d3cff5b0ddb762a3a1ee25842c1b928d58289c90
“yongjian.tek” <yongjian.tek@grabtaxi.com>
2023-07-08 13:55:09 +0800

    add java

--- Java Programming and Software Engineering Fundamentals/1. Programming Foundations with Javascript, HTML, and CSS/Week 3/Practise/Practise 1/my_page.js
+++ Java Programming and Software Engineering Fundamentals/1. Programming Foundations with Javascript, HTML, and CSS/Week 3/Practise/Practise 1/my_page.js
@@ -12,3 +12,10 @@ function doConfirm(){
     }
     alert(message)
 }
+
+function swapDiv1And2(){
+    var d1 = document.getElement('div1')
+    var d2 = document.getElement('div2')
+    d1.className = 'div2'
+    d2.className = 'div1'
+}
