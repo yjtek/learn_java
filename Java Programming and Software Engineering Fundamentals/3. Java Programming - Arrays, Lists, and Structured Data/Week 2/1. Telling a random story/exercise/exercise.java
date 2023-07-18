eff870dd3028aecdf16823aacbb1aab9c6df83d9
“yongjian.tek” <yongjian.tek@grabtaxi.com>
2023-07-18 10:58:45 +0800

    sync data

--- Java Programming and Software Engineering Fundamentals/3. Java Programming - Arrays, Lists, and Structured Data/Week 2/1. Telling a random story/exercise/exercise.java
+++ Java Programming and Software Engineering Fundamentals/3. Java Programming - Arrays, Lists, and Structured Data/Week 2/1. Telling a random story/exercise/exercise.java
@@ -16,7 +16,7 @@ public class exercise {
             myWords.clear();
             myFreqs.clear();
 
-            FileResource fr = new FileResource("testwordfreqs.txt");
+            FileResource fr = new FileResource("likeit.txt");
             for (String word: fr.words()){
                 word = word.toLowerCase();
                 int idx = myWords.indexOf(word);
@@ -58,11 +58,67 @@ public class exercise {
 
     }
     
-    public class 
+    public class CharactersInPlay {
+        private ArrayList<String> characterNames;
+        private ArrayList<Integer> characterCounts;
+
+        public CharactersInPlay() {
+            characterNames = new ArrayList<String>();
+            characterCounts = new ArrayList<Integer>();
+        }
+
+        public void update(String person){
+            int idx = characterNames.indexOf(person);
+            if (idx == -1) {
+                characterNames.add(person);
+                characterCounts.add(1);
+            }
+            else {
+                int count = characterCounts.get(idx);
+                characterCounts.set(idx, count+1);
+            }
+        }
+
+        public void findAllCharacters(){
+            characterNames.clear();
+            characterCounts.clear();
+
+            // FileResource fr = new FileResource("macbethSmall.txt");
+            // FileResource fr = new FileResource("macbeth.txt");
+            FileResource fr = new FileResource("likeit.txt");
+
+            for (String line: fr.lines()){
+                int idx = line.indexOf(".");
+                if (idx != -1){
+                    String characterName = line.substring(0, idx);
+                    update(characterName);
+                }
+            }
+        }
+
+        public void tester(){
+            findAllCharacters();
+            characterWithNumParts(10,15);
+        }
+
+        public void characterWithNumParts(int num1, int num2){
+            assert num1 <= num2;
+            findAllCharacters();
+            for (int i=0; i < characterNames.size(); i++){
+                if (characterCounts.get(i) >= num1 && characterCounts.get(i) <= num2) {
+                    System.out.println("Character Name: " + characterNames.get(i) + " || Character line count: " + characterCounts.get(i));
+                }            
+            }
+        }
+    }
 
     public static void main(String[] args){
         exercise e = new exercise();
-        WordFrequencies wf = e.new WordFrequencies();
-        wf.tester();
-    }    
+        
+        // WordFrequencies wf = e.new WordFrequencies();
+        // wf.tester();
+
+        CharactersInPlay cip = e.new CharactersInPlay();
+        cip.tester();
+    }
 }
