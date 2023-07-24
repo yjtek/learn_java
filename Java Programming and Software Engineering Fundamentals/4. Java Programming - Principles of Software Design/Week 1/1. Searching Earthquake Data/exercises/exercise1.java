80186a4f3b16f698bc0f5a8b1b14bd48a42d6e9c
“yongjian.tek” <yongjian.tek@grabtaxi.com>
2023-07-24 12:23:27 +0800

    setup

---
+++ Java Programming and Software Engineering Fundamentals/4. Java Programming - Principles of Software Design/Week 1/1. Searching Earthquake Data/exercises/exercise1.java
@@ -0,0 +1,8 @@
+import exercise1.edu.duke.*;
+import exercise1.SearchingEarthquakeDataStarterProgram.*;
+
+public class exercise1 {
+    public static void main(String[] args){
+        System.out.println("printing string");
+    }
+}
